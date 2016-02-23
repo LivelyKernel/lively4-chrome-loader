41ebe51f9bc0c394c0511552c1bbb06f6df72cd7
athomschke <astrid.thomschke@gmail.com>
2016-02-23 11:45:54 +0100

    removed unneccessary init steps

--- scripts/loadComponent.js
+++ scripts/loadComponent.js
@@ -12,34 +12,18 @@ function loadTemplate (partNamesString, url) {
     setTimeout(function () {
       loadBabel();
       loadLively4();
-      loadAce();
       var partName = partNamesString.split(' ')[0];
-      var requiredTemplates = [partName].concat(templateNamesFromString(partNamesString));
-      loadPartsViaLinkTag(requiredTemplates);
+      addLinkTagForPart(partName);
       mountPart(partName);
     }, 1000)
   }, 1000)
 }
 
-function templateNamesFromString(templateNamesString) {
-  // allows loading (not mounting) additional modules with "temlateName (requiredTemplate1, requiredTemplate2)"
-  var templatesNamesMatches = templateNamesString.match(/\((.*)\)/);
-  if (templatesNamesMatches && templatesNamesMatches.length > 0) {
-    return [1].split(', ').map(function (templateNameString) {
-      return templateNameString.trim();
-    })
-  } else {
-    return [];
-  }
-}
-
-function loadPartsViaLinkTag (partNames) {
-  partNames.forEach(function (partName) {
-    var linkTag = document.createElement('link');
-    linkTag.setAttribute('rel', 'import');
-    linkTag.setAttribute('href', lively4Url + 'templates/' + partName + '.html');
-    document.head.appendChild(linkTag);
-  })
+function addLinkTagForPart (partName) {
+  var linkTag = document.createElement('link');
+  linkTag.setAttribute('rel', 'import');
+  linkTag.setAttribute('href', lively4Url + 'templates/' + partName + '.html');
+  document.head.appendChild(linkTag);
 }
 
 function mountPart(partIdentifierString) {
@@ -51,13 +35,6 @@ function mountPart(partIdentifierString) {
     type: 'loadedInTab',
     payload: config.tab
   });
-}
-
-function loadAce() {
-  var aceNode = document.createElement('script');
-  aceNode.setAttribute('type', 'text/javascript');
-  aceNode.setAttribute('src', lively4Url + 'src/external/ace.js');
-  document.head.appendChild(aceNode);
 }
 
 function loadJQuery () {
@@ -82,16 +59,16 @@ function loadSystem () {
 function loadBabel () {
   var babelLoaderNode = document.createElement('script');
   babelLoaderNode.setAttribute('type', 'text/javascript');
-  babelLoaderNode.innerHTML = '' + '\n' +
-    "window.lively4Url = '" + lively4Url + "';\n" +
-    "System.paths['babel'] = '" + lively4Url + "src/external/babel-browser.js'" + ';\n' +
-    "System.config({" + '\n' +
-      "transpiler: 'babel'," + '\n' +
-      "babelOptions: { }," + '\n' +
-      "map: {" + '\n' +
-          "babel: '" + lively4Url + "src/external/babel-browser.js'" + '\n' +
-      "}" + '\n' +
-    "});"
+  babelLoaderNode.innerHTML = `
+    window.lively4Url = '${lively4Url}';
+    System.paths['babel'] = '${lively4Url}src/external/babel-browser.js;'
+    System.config({
+      transpiler: 'babel',
+      babelOptions: { },
+      map: {
+        babel: '${lively4Url}src/external/babel-browser.js'
+      }
+    })`
   document.head.appendChild(babelLoaderNode);
   System.config({
     baseURL: lively4Url + 'draft/'
