5b7d0a7b8f3691dfd7eae9cb6112eb215f672c7d
Jens Lincke <jens.lincke@hpi.uni-potsdam.de>
2016-03-03 17:15:11 +0100

    forgot file

---
+++ configs.js
@@ -0,0 +1,28 @@
+
+function withConfig(mutateConfigCB) {
+	chrome.storage.sync.get(["lively4"], function(configs) {
+		mutateConfigCB(ensureConfig(configs), function() {
+			chrome.storage.sync.set(configs) // for saving inbetween	
+		})
+		chrome.storage.sync.set(configs)
+	})	
+}
+
+function ensureConfig(configs) {
+	configs.lively4 = configs.lively4 || {}
+	configs.lively4.location = configs.lively4.location || "http://lively-kernel.org/lively4/"
+	configs.lively4.hosts = configs.lively4.hosts || {}
+	return configs.lively4
+}
+
+function ensureHost(config, host) {
+	config.hosts[host] = config.hosts[host] || {active: false}
+	return config.hosts[host]
+}
+
+function setIconPath(path) {
+	chrome.browserAction.setIcon({
+        path: path
+    });
+}
+
